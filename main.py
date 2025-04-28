6df03786ed5b0f9957b10bbcd03c33438020eca5
Sacriflces <Jmhservice@gmail.com>
2025-04-28 13:38:23 -0400

    Finished printing the report

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_nums, sort_char_dict
 
 def get_book_text(filePath):
     text = ""
     with open(filePath) as f:
+        print(f"Analyzing book found at {filePath}...")
         text = f.read()
     
     return text
@@ -10,9 +11,20 @@ def get_book_text(filePath):
 
 
 def main():
+    print("============ BOOKBOT ============")
     text = get_book_text("./books/frankenstein.txt")
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    char_nums = get_char_nums(text)
+    sorted_char_list = sort_char_dict(char_nums)
+    print("--------- Character Count -------")
+    for charDict in sorted_char_list:
+        for char, count in charDict.items():
+            if not char.isalpha():
+                continue
+            print(f"{char}: {count}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,23 @@
 def get_num_words(text):
     return len(text.split())
+
+def get_char_nums(text):
+    char_counts = {}
+    for char in text:
+        char_lower = char.lower()
+        if char_lower in char_counts:
+            char_counts[char_lower] += 1
+        else:
+            char_counts[char_lower] = 1
+    return char_counts
+
+def sort_char_dict(dict):
+    sorted_list = sorted([{key: value} for key, value in dict.items()], key=get_first_dict_value, reverse=True)
+
+    return sorted_list
+
+def get_first_dict_value(dict):
+    if len(dict) == 0:
+        return None
+    return dict[list(dict)[0]]
+    

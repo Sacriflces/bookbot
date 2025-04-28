02c48b6e319513080d5ff1d41c4e96e656196017
Sacriflces <Jmhservice@gmail.com>
2025-04-28 13:44:53 -0400

    Added CLI arguments to the project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import get_num_words, get_char_nums, sort_char_dict
-
+import sys
 def get_book_text(filePath):
     text = ""
     with open(filePath) as f:
@@ -11,8 +11,13 @@ def get_book_text(filePath):
 
 
 def main():
+    if(len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     print("============ BOOKBOT ============")
-    text = get_book_text("./books/frankenstein.txt")
+    text = get_book_text(book_path)
     num_words = get_num_words(text)
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
